d09e6b5d79ec4e220de9c0b2d01c9963804dd141
johnwav <102820657+johnwav@users.noreply.github.com>
2023-07-19 13:52:06 +0100

    feat: chat pages+ layout

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,5 +1,11 @@
 import { SignIn } from "@/components/Action/Action";
+import { GoogleSignIn } from "@/components/Google/Google";
 
 export default function Test() {
-  return <SignIn text="Sign Up" />;
+  return (
+    <div className="max-w-[1440px] m-auto flex flex-col gap-8 items-center justify-center border p-2 bg-slate-300">
+      <SignIn text="Sign in" />
+      <GoogleSignIn text="Continue with Google Instead" />
+    </div>
+  );
 }
