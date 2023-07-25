c1f7e8993e78270f522b53b20b11c8bfede5cc94
johnwav <102820657+johnwav@users.noreply.github.com>
2023-07-25 15:43:52 +0100

    chore: home page reset

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 
-import {Signin} from "../components/Authentication/Signin";
 export default function Home() {
   return (
     <main className="">
-      <Signin/>
+      Home
     </main>
   );
 }
