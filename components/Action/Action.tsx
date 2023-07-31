aed69516a865444d266eb336a0f9318954ff8cb5
johnwav <102820657+johnwav@users.noreply.github.com>
2023-07-31 12:07:38 +0100

    added loading ux to sigiin button

--- components/Action/Action.tsx
+++ components/Action/Action.tsx
@@ -1,12 +1,11 @@
 import { IButtonProps } from "@/types/types";
 
-
-export function SignIn({ text }: IButtonProps) {
-
-
+export function SignIn({ text, loading }: IButtonProps) {
   return (
     <button
       type="submit"
+      disabled={loading}
+      style={{ opacity: loading ? 0.5 : 1 }}
       className="text-white text-[14px] h-[42px] w-full max-w-[400px] rounded-lg bg-green"
     >
       {text}
