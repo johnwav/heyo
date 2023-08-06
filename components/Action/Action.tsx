e6e417dead21ee8025707a001c74afb28a67cee8
johnwav <102820657+johnwav@users.noreply.github.com>
2023-08-06 16:12:04 +0100

    feat: loader component

--- components/Action/Action.tsx
+++ components/Action/Action.tsx
@@ -1,5 +1,4 @@
 import { IButtonProps } from "@/types/types";
-import { useState } from "react";
 import { PulseLoader } from "react-spinners";
 
 export function SignIn({ text, loading }: IButtonProps) {
@@ -7,18 +6,23 @@ export function SignIn({ text, loading }: IButtonProps) {
     <button
       type="submit"
       disabled={loading}
-      style={{ opacity: loading ? 0.5 : 1 }}
-      className="text-white text-[14px] h-[42px] w-full max-w-[400px] rounded-lg bg-green"
+      // style={{ opacity: loading ? 0.5 : 1 }}
+      className="text-white text-[14px] h-[42px] w-full max-w-[400px] rounded-lg bg-green flex items-center justify-center"
     >
       {loading ? <PulseLoader /> : text}
     </button>
   );
 }
 
-export function SignUp({ text }: IButtonProps) {
+export function SignUp({ text, loading }: IButtonProps) {
   return (
-    <button className="text-white text-[18px] h-[52px] w-full max-w-[400px] rounded-lg bg-green">
-      {text}
+    <button
+      type="submit"
+      disabled={loading}
+      // style={{ opacity: loading ? 0.5 : 1 }}
+      className="text-white text-[14px] h-[42px] w-full max-w-[400px] rounded-lg bg-green flex items-center justify-center"
+    >
+      {loading ? <PulseLoader /> : text}
     </button>
   );
 }
