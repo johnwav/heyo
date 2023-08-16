c39cfae81225df754ff72fee0d1b9d8f8f07c163
johnwav <102820657+johnwav@users.noreply.github.com>
2023-08-16 13:02:50 +0000

    feat: layer for texts

--- components/ChatArea/ChatArea.tsx
+++ components/ChatArea/ChatArea.tsx
@@ -4,15 +4,17 @@ import ChatTextFeild from "../ChatTextFeild/ChatTextFeild";
 export default function ChatArea() {
   return (
     <div className="h-full relative">
+      <div className="absolute z-[900] w-full h-auto" >
+        text layer.
+      </div>
       <Image
         src="/assets/mask.png"
         alt="mask"
         width={0}
         height={0}
         sizes="100vw"
-        className="w-full h-full object-cover absolute z-0"
+        className="w-full h-full object-cover absolute z-[0]"
       />
-
       <div className=" w-full h-auto absolute px-2 bottom-3">
         <ChatTextFeild />
       </div>
