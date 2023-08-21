3a919a8e9199acc9d3665be5b2f25a5b770a6a2d
johnwav <102820657+johnwav@users.noreply.github.com>
2023-08-21 10:09:42 +0000

    update about route

---
+++ app/api/updateAbout/route.ts
@@ -0,0 +1,25 @@
+import dbConnect from "@/utils/database";
+import User from "@/models/user";
+
+export const POST = async (req: Request, res: Response) => {
+  try {
+    const { sessionId, newAbout } = await req.json(); // Get session ID from the request body
+    console.log(sessionId, newAbout)
+    await dbConnect();
+
+    const user = await User.findOneAndUpdate(
+      { _id: sessionId },
+      { $set: { about: newAbout } }, // Update the "about" field
+      { new: true } // Return the updated document
+    );
+    if (user) {
+      return new Response(JSON.stringify(user), { status: 200 });
+    } else {
+      return new Response("User not found", { status: 404 });
+    }
+  } catch (error) {
+    console.error("Error updating user:", error);
+    return new Response("internal server error", { status: 500 });
+  }
+};
+
