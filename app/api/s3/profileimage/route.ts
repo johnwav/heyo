9b8421d6e81900e4535515cb2d2f091b56ae7be9
johnwav <102820657+johnwav@users.noreply.github.com>
2023-08-23 09:49:45 +0000

    feat: route for api to post image link to db

--- app/api/s3/profileimage/route.ts
+++ app/api/s3/profileimage/route.ts
@@ -1,53 +1,20 @@
 import User from "@/models/user";
 import dbConnect from "@/utils/database";
-import S3 from "aws-sdk/clients/s3";
+import { useS3Upload } from "@/utils/useS3upload";
 
-const s3 = new S3({
-  region: "eu-central-1",
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_ACCESS_KEY_SECRET,
-  signatureVersion: "v4",
-});
-
-export const config = {
-  api: {
-    bodyParser: {
-      sizeLimit: "24mb", // Set desired value here
-    },
-  },
-};
-
-export const GET = async (req: Request, res: Response) => {
+export const POST = async (req: Request, res: Response) => {
   try {
-    //@ts-ignore
-    const { name, type, sessionId } = await req.json;
-
-    const fileParams = {
-      Bucket: process.env.AWS_BUCKET_NAME,
-      Key: name,
-      Expires: 600,
-      ContentType: type,
-      ACL: "public-read",
-    };
-    const url = await s3.getSignedUrlPromise("putObject", fileParams);
-    // console.log(url)
-    return new Response(JSON.stringify({ url }), { status: 200 });
-
-
-    // here we append the user image url to our db
-    if (url) {
-      await dbConnect();
-      const user = await User.findOneAndUpdate(
-        { _id: sessionId },
-        { profileImage: url }, // Update the "profileImage" field
-        { new: true } // Return the updated document
-      );
-
-      if (user) {
-        return new Response(JSON.stringify({ user, url }), { status: 200 });
-      } else {
-        return new Response("User not found", { status: 404 });
-      }
+    const { sessionId, url } = await req.json();
+    await dbConnect();
+    const user = await User.findOneAndUpdate(
+      { _id: sessionId },
+      { $set: { profileImage: url } }, // Update the "profileImage" field
+      { new: true } // Return the updated document
+    );
+    if (user) {
+      return new Response(JSON.stringify({ user }), { status: 200 });
+    } else {
+      return new Response("User not found", { status: 404 });
     }
   } catch (error) {
     console.error("Error updating user:", error);
