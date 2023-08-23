3303115b2cbbb3019a60392d1f7df9d3cf719ed7
johnwav <102820657+johnwav@users.noreply.github.com>
2023-08-23 09:49:29 +0000

    test upload component

--- components/testupload/tetstupload.tsx
+++ components/testupload/tetstupload.tsx
@@ -1,13 +1,13 @@
 import { RootState } from "@/store/userStore";
 import { ChangeEvent, useState } from "react";
 import { useSelector } from "react-redux";
-import axios, { Axios } from "axios";
+import { useS3Upload } from "@/utils/useS3upload";
+import { error } from "console";
 
 export default function TestUpload() {
-  const [file, setFile] = useState<any>();
+  const [file, setFile] = useState<File>();
   const [uploadingStatus, setUploadingStatus] = useState<any>();
   const [uploadedFile, setUploadedFile] = useState<string>();
-  const BUCKET_URL = "https://heyochat.s3.eu-noth-1.amazonaws.com/";
 
   const selectFile = (e: ChangeEvent<HTMLInputElement>) => {
     console.log(e.target.files);
@@ -16,60 +16,30 @@ export default function TestUpload() {
   };
   const { _id: id } = useSelector((state: RootState) => state.user);
 
-  const uploadFile = async () => {
-    setUploadingStatus("Uploading the file to AWS S3");
-
-    if (file) {
-      // const response = await fetch("/api/s3/profileimage", {
-      //   method: "POST",
-      //   headers: {
-      //     "Content-Type": file.type,
-      //     "Access-Control-Allow-Origin": "*",
-      //   },
-      //   body: JSON.stringify({
-      //     sessionId: id,
-      //     name: file.name,
-      //     type: file.type,
-      //   }),
-      // });
-      // const responseData = await response.json();
-      // console.log(responseData);
-
-      // const url = responseData.url;
-      // const data = await fetch(url, {
-      //   method: "PUT",
-      //   headers: {
-      //     "Content-type": file.type,
-      //     "Access-Control-Allow-Origin": "*",
-      //   },
-      //   body: file,
-      // });
-
-      setUploadingStatus("Uploading the file to AWS S3");
-
-      let { data } = await axios.get("/api/s3/profileimage", {
-        //@ts-ignore
-        name: file.name,
-        type: file.type,
-        sessionId: id,
-      });
-
-      console.log(data);
-
-      const url = data.url;
-      let { data: newData } = await axios.put(url, file, {
-        headers: {
-          "Content-type": file.type,
-          "Access-Control-Allow-Origin": "*",
-        },
-      });
-
-      setUploadedFile(BUCKET_URL + file.name);
-      setFile(null);
+  const handleUpload = async () => {
+    if (!file) return;
+    try {
+      const result = await useS3Upload(file);
+      if (result?.error) return;
+      if (result?.url) {
+        console.log("file uploaded");
+        const sessionId = id;
+        const url = result.url;
+        const data = await fetch("/api/s3/profileimage", {
+          method: "POST", // Use the appropriate HTTP method (POST, GET, etc.)
+          headers: {
+            "Content-Type": "application/json",
+          },
+          //@ts-ignore
+          body: JSON.stringify({ sessionId, url }),
+        });
+        const response = await data.json();
+        console.log(response);
+        setUploadedFile(result.url);
+      }
+    } catch (error) {
+      return error;
     }
-
-    // file && setUploadedFile(BUCKET_URL + file.name);
-    // setFile(null);
   };
 
   return (
@@ -81,7 +51,7 @@ export default function TestUpload() {
           <p>Selected file: {file.name}</p>
 
           <button
-            onClick={uploadFile}
+            onClick={handleUpload}
             className=" bg-purple-500 text-white p-2 rounded-sm shadow-md hover:bg-purple-700 transition-all"
           >
             Upload a File!
@@ -89,7 +59,7 @@ export default function TestUpload() {
         </>
       )}
       {uploadingStatus && <p>{uploadingStatus}</p>}
-      {uploadedFile && <img src={uploadedFile} />}{" "}
+      {uploadedFile && <img src={uploadedFile} />}
     </div>
   );
 }
