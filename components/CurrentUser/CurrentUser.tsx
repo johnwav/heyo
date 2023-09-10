1af0e389c8f68b796fd58b0d600c4276c978a634
Johnwav <johnwav@users.noreply.github.com>
2023-09-10 12:20:50 +0100

    button cannot appear as a decendant of button error

--- components/CurrentUser/CurrentUser.tsx
+++ components/CurrentUser/CurrentUser.tsx
@@ -11,36 +11,38 @@ export default function CurrentUser({
   };
 
   return (
-    <button
-      onClick={openModal}
-      className="flex items-center justify-start gap-[22px] text-[22px] w-full max-w-[635px]"
-    >
-      <Image
-        src={profileImage}
-        alt="Profile Image"
-        width={72}
-        height={72}
-        className="rounded-full hover:scale-110"
-      />
-      <h1 className="text-black font-[600]">@{username}</h1>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 256 256"
-        id="IconChangeColor"
-        height="20"
-        width="20"
-        className="hover:scale-110"
+    <>
+      <button
+        onClick={openModal}
+        className="flex items-center justify-start gap-[22px] text-[22px] w-full max-w-[635px]"
       >
-        <rect width="256" height="256" fill="none"></rect>
-        <polyline
-          points="208 96 128 176 48 96"
-          fill="none"
-          stroke="#000"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="12"
-        ></polyline>
-      </svg>
-    </button>
+        <Image
+          src={profileImage}
+          alt="Profile Image"
+          width={72}
+          height={72}
+          className="rounded-full hover:scale-110"
+        />
+        <h1 className="text-black font-[600]">@{username}</h1>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 256 256"
+          id="IconChangeColor"
+          height="20"
+          width="20"
+          className="hover:scale-110"
+        >
+          <rect width="256" height="256" fill="none"></rect>
+          <polyline
+            points="208 96 128 176 48 96"
+            fill="none"
+            stroke="#000"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="12"
+          ></polyline>
+        </svg>
+      </button>
+    </>
   );
 }
