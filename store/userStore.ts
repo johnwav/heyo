834f89454eddeb955c3d4ed00362c759a87ede2d
Johnwav <johnwav@users.noreply.github.com>
2023-10-08 17:16:05 +0100

    Feat: Added logic for stroing zim instance

--- store/userStore.ts
+++ store/userStore.ts
@@ -1,15 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from "@/features/user/userSlice"
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "@/features/user/userSlice";
+import zimReducer from "@/features/message/zimSlice";
 
 export const userStore = configureStore({
   reducer: {
-    user: userReducer
+    user: userReducer,
+    zim: zimReducer,
   },
-})
+});
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof userStore.getState>
+export type RootState = ReturnType<typeof userStore.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof userStore.dispatch
-
-
+export type AppDispatch = typeof userStore.dispatch;
