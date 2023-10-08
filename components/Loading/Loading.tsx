61470ca1788a6f41b10f6920bceb752b3c4b530e
Johnwav <johnwav@users.noreply.github.com>
2023-10-08 18:06:52 +0100

    Feat: Priority option to images

--- components/Loading/Loading.tsx
+++ components/Loading/Loading.tsx
@@ -4,7 +4,7 @@ import { PulseLoader } from "react-spinners";
 export default function Loading() {
   return (
     <div className="w-screen h-screen bg-white flex flex-col gap-6 items-center justify-center">
-      <Image src="/assets/logo.png" alt="logo " width={300} height={300} />
+      <Image src="/assets/logo.png" alt="logo " width={300} height={300} priority />
       <h1 className="italic text-xl">Getting things ready...</h1>
       <PulseLoader color="white" />
     </div>
