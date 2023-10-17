59748979e0b29e43e63ff12d2f0c701ddc4fd3db
Johnwav <johnwav@users.noreply.github.com>
2023-10-17 22:57:24 +0100

    Feat: Agora init

--- app/api/getuser/route.ts
+++ app/api/getuser/route.ts
@@ -1,5 +1,27 @@
 import dbConnect from "@/utils/database";
 import User from "@/models/user";
+import {
+  RtmRole,
+  RtmTokenBuilder,
+} from "agora-access-token";
+
+function getRtmToken(userId: string) {
+  const appID = process.env.NEXT_PUBLIC_AGORA_APP_ID!;
+  const appCertificate = process.env.NEXT_PUBLIC_AGORA_APP_CERT!;
+  const account = userId;
+  const expirationTimeInSeconds = 3600;
+  const currentTimeStamp = Math.floor(Date.now() / 1000);
+  const privilegeExpiredTs = currentTimeStamp + expirationTimeInSeconds;
+
+  const token = RtmTokenBuilder.buildToken(
+    appID,
+    appCertificate,
+    account,
+    RtmRole.Rtm_User,
+    privilegeExpiredTs
+  );
+  return token;
+}
 
 export const POST = async (req: Request, res: Response) => {
   try {
@@ -9,13 +31,13 @@ export const POST = async (req: Request, res: Response) => {
     // Find user by session ID
     const user = await User.findOne({ _id: sessionId });
     if (user) {
-      console.log("user from api", user)
-      return new Response(JSON.stringify(user), { status: 200 });
+      const token = getRtmToken("72");
+      console.log("user from api", user, "token", token);
+      return new Response(JSON.stringify({user, token}), { status: 200 });
     } else {
       return new Response("User not found", { status: 404 });
     }
   } catch (error) {
     return new Response("Error fetching user data", { status: 500 });
   }
-
 };
